4c9111090623023e791ce007e1746a0493fd9a9f
Chris Chiang <chris23chiang@gmail.com>
2021-05-08 22:57:28 +0800

    add word filter

--- read.py
+++ read.py
@@ -23,3 +23,11 @@ for d in data:
 print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言內有提到good')
+print(good[0])
+print(good[1])
