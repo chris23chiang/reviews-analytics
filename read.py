801272160d0cd9923a88fa0cbaba28dc672d9d0d
Chris Chiang <chris23chiang@gmail.com>
2021-05-08 23:13:02 +0800

    list comprehension

--- read.py
+++ read.py
@@ -24,10 +24,13 @@ print('一共有', len(new), '筆留言長度小於100')
 print(new[0])
 print(new[1])
 
-good = []
-for d in data:
-	if 'good' in d:
-		good.append(d)
+#good = []
+#for d in data:
+#	if 'good' in d:
+#		good.append(d)
+
+good = [d for d in data if 'good' in d] #list comprehension
 print('一共有', len(good), '筆留言內有提到good')
 print(good[0])
 print(good[1])
+
